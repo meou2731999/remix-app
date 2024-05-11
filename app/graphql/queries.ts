ef30fd6543e3572b8543f8267c89658e1cfcffc9
Quan Le <meou2731999@gmail.com>
2024-05-12 01:54:51 +0700

    Update query

--- app/graphql/queries.ts
+++ app/graphql/queries.ts
@@ -4,9 +4,31 @@ import { gql } from "@apollo/client/core";
 export const GET_ALL_COUNTRIES = gql`
   query GetAllCountries {
     countries {
+      awsRegion
+      capital
       code
-      name
+      continent {
+        name
+        code
+      }
+      currencies
+      currency
       emoji
+      emojiU
+      languages {
+        name
+        native
+      }
+      name
+      native
+      phone
+      phones
+      states {
+        name
+      }
+      subdivisions {
+        name
+      }
     }
   }
 `;
