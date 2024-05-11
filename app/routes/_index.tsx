8391854a9e7609e1baf27c8fd053ea02c74c1b6c
Quan Le <meou2731999@gmail.com>
2024-05-12 01:18:15 +0700

    update index

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,29 @@
+import { useQuery } from "@apollo/client";
 import type { MetaFunction } from "@remix-run/node";
+import type { Country } from "~/graphql/__generated__/graphql";
+import { GET_ALL_COUNTRIES } from "~/graphql/queries";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Countries" },
+    { name: "description", content: "Query countries with remix and graphQL" },
   ];
 };
 
 export default function Index() {
+  const { loading, error, data } = useQuery(GET_ALL_COUNTRIES);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+      {data.countries.map((country: Country) => (
+        <div key={country.code}>
+          <h2>
+            {country.emoji} {country.name}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
