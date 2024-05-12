36c0b259d7d58681b00e03715afaf840d22bf441
Quan Le <meou2731999@gmail.com>
2024-05-12 16:56:45 +0700

    Update code coverage view

--- jest.config.cjs
+++ jest.config.cjs
@@ -12,5 +12,15 @@ module.exports = {
         '/node_modules/(?!(@web3-storage)/)', // Adjust this pattern to include specific modules
     ],
     "setupFilesAfterEnv": ["@testing-library/jest-dom"],
-
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'html'],
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80
+        }
+    }
 };
