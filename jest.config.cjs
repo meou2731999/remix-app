eb552108361a62006c0c12c451b60cf8cb35fdca
Quan Le <meou2731999@gmail.com>
2024-05-13 11:29:02 +0700

    Update config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,6 @@
 // jest.config.js
+const esModules = ['@web3-storage', '@apollo'].join('|'); // Adjust this pattern to include specific modules
+
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
@@ -9,9 +11,12 @@ module.exports = {
         '^.+\\.js?$': 'babel-jest', // For non-TypeScript files
     },
     transformIgnorePatterns: [
-        '/node_modules/(?!(@web3-storage)/)', // Adjust this pattern to include specific modules
+        `/node_modules/(?!${esModules})`
     ],
     "setupFilesAfterEnv": ["@testing-library/jest-dom"],
+    moduleNameMapper: {
+        '^~/(.*)$': '<rootDir>/app/$1'
+    },
     collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'html'],
